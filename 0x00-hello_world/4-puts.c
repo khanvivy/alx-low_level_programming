af7c77f32b6c2bc50055b723b97496e60a49934a
khanvivy <vivykhan@gmail.com>
2022-03-10 05:51:00 -0800

    100-intel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,11 @@
 
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+
 
 int main(void)
 
@@ -19,5 +16,4 @@ int main(void)
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
-
 }
