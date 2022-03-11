1deeca48ed42eaa74cf3ad53f6a7a8f0af3542aa
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-11 15:28:04 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,51 +1,26 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *   * main - Prints a serie of numbers with commas
- *
- *     *
- *
- *       * Return: Always (Success);
- *
- *         */
-
+  * main - Prints a serie of numbers with commas
+  *
+  * Return: Always (Success);
+  */
 int main(void)
-
 {
+	int c;
 
-		int c;
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
 
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
+	putchar('\n');
 
-			for (c = '0'; c <= '9'; c++)
-
-					{
-
-								putchar(c);
-
-
-
-										if (c != '9')
-
-													{
-
-																	putchar(',');
-
-																				putchar(' ');
-
-																						}
-
-											}
-
-
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	return (0);
 }
