6deae74e54c9a5dc044da004a737d14e21654976
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-18 03:15:30 +0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,27 +1,14 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- *
- *  * print_line - draws a straight line (n) characters long.
- *
- *   * @n: the number of underscores to print.
- *
- *    */
-
+ * print_line - draws a straight line (n) characters long.
+ * @n: the number of underscores to print.
+ */
 void print_line(int n)
-
 {
+	int i;
 
-		int i;
-
-
-
-			if (n > 0)
-
-						for (i = 0; i < n; i++)
-
-										_putchar('_');
-
-				_putchar('\n');
-
+	if (n > 0)
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	_putchar('\n');
 }
