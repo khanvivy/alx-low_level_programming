092ed500edc2b528446bc2e43e6d5417618358de
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-18 03:09:40 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,29 +1,15 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- *
- *  * print_numbers - prints the digits 1-9
- *
- *   */
-
+ * print_numbers - prints the digits 1-9
+ */
 void print_numbers(void)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			while (i <= 9)
-
-					{
-
-								_putchar(i + '0');
-
-										++i;
-
-											}
-
-				_putchar('\n');
-
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		++i;
+	}
+	_putchar('\n');
 }
