06c2583f8c3f318c405e4560f7a1af8fea8ac9af
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-21 19:10:01 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,43 +1,22 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ *print_rev - print strings in reverse
+ *@s: characters
  *
- *  *print_rev - print strings in reverse
- *
- *   *@s: characters
- *
- *    *
- *
- *     *Return: nothing
- *
- *      */
-
-
+ *Return: nothing
+ */
 
 void print_rev(char *s)
-
 {
+int i;
 
-	int i;
-
-
-
-	for (i = 0; s[i] != '\0'; i++)
-
-		;
-
-	while (i)
-
-	{
-
-		_putchar(s[i - 1]);
-
-		i--;
-
-	}
-
-	_putchar('\n');
-
+for (i = 0; s[i] != '\0'; i++)
+;
+while (i)
+{
+_putchar(s[i - 1]);
+i--;
+}
+_putchar('\n');
 }
