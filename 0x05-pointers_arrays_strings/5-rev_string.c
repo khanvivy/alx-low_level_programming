405c04305a16707a93384286d25c346d77df47fd
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-21 19:27:05 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,55 +1,28 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ *rev_string - print string in reverse
+ *@s: pointer
  *
- *  *rev_string - print string in reverse
- *
- *   *@s: pointer
- *
- *    *
- *
- *     *Return: nothing
- *
- *      */
-
-
+ *Return: nothing
+ */
 
 void rev_string(char *s)
-
 {
+int i, l, len = 0;
+char c;
 
-	int i, l, len = 0;
+while (s[len])
+{
+len++;
+l = len - 1;
+}
 
-	char c;
-
-
-
-	while (s[len])
-
-	{
-
-		len++;
-
-		l = len - 1;
-
-	}
-
-
-
-	for (i = 0; i < len / 2; i++)
-
-	{
-
-		c = s[i];
-
-		s[i] = s[l];
-
-		s[l] = c;
-
-		l--;
-
-	}
-
+for (i = 0; i < len / 2; i++)
+{
+c = s[i];
+s[i] = s[l];
+s[l] = c;
+l--;
+}
 }
