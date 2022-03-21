3619eec4e9a2a08ed95ef0c79b622e42694220b2
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-21 13:37:03 +0300

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,35 +1,18 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ *swap_int - swap numbers function
+ *@a: integer
+ *@b: integer
  *
- *  *swap_int - swap numbers function
- *
- *   *@a: integer
- *
- *    *@b: integer
- *
- *     *
- *
- *      *Return: nothing
- *
- *       */
-
-
+ *Return: nothing
+ */
 
 void swap_int(int *a, int *b)
-
 {
+int temp;
 
-	int temp;
-
-
-
-	temp = *a;
-
-	*a = *b;
-
-	*b = temp;
-
+temp = *a;
+*a = *b;
+*b = temp;
 }
