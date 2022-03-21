2e4a4d30128fd5bd9c019273bd446108e92f2f7a
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-21 19:33:42 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,73 +1,37 @@
-#include "holberton.h"
-
+#include "main.h"
 #include <stdio.h>
 
-
-
 /**
+ *puts_half - print half string
+ *@str: string
  *
- *  *puts_half - print half string
- *
- *   *@str: string
- *
- *    *
- *
- *     *Return: nothing
- *
- *      */
-
-
+ *Return: nothing
+ */
 
 void puts_half(char *str)
-
 {
+int i = 0;
+int j = 0;
 
-	int i = 0;
-
-	int j = 0;
-
-
-
-	while (str[i] != '\0')
-
-	{
-
-		i++;
-
-	}
-
-	if (i % 2 == 0)
-
-	{
-
-		for (j = 0; str[j] != '\0'; j++)
-
-		{
-
-			if (j >= i / 2)
-
-				_putchar(str[j]);
-
-		}
-
-	}
-
-	else
-
-	{
-
-		for (j = 0; str[j] != '\0'; j++)
-
-		{
-
-			if (j > i / 2)
-
-				_putchar(str[j]);
-
-		}
-
-	}
-
-	_putchar('\n');
-
+while (str[i] != '\0')
+{
+i++;
+}
+if (i % 2 == 0)
+{
+for (j = 0; str[j] != '\0'; j++)
+{
+if (j >= i / 2)
+_putchar(str[j]);
+}
+}
+else
+{
+for (j = 0; str[j] != '\0'; j++)
+{
+if (j > i / 2)
+_putchar(str[j]);
+}
+}
+_putchar('\n');
 }
