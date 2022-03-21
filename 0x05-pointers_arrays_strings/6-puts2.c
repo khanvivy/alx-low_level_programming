505eea1c0547bf4576dacae1e65a66d0ff71afbd
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-21 19:29:31 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,43 +1,22 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ *puts2 - print every other charachter of a string
+ *@str: string
  *
- *  *puts2 - print every other charachter of a string
- *
- *   *@str: string
- *
- *    *
- *
- *     *Return: nothing
- *
- *      */
-
-
+ *Return: nothing
+ */
 
 void puts2(char *str)
-
 {
+int i;
+i = 0;
 
-	int i;
-
-	i = 0;
-
-
-
-	while (str[i])
-
-	{
-
-		if (i % 2 == 0)
-
-			_putchar(str[i]);
-
-		i++;
-
-	}
-
-	_putchar('\n');
-
+while (str[i])
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
 }
