012382f0c6c4cb92758cb4e1c64cb8fed8d9b1f2
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-21 19:51:09 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,39 +1,20 @@
-#include "holberton.h"
-
+#include "main.h"
 #include <stdio.h>
 
-
-
 /**
+ *_strcpy - copy string including the terminating null byte
+ *@dest: destination of string
+ *@src: source of string
  *
- *  *_strcpy - copy string including the terminating null byte
- *
- *   *@dest: destination of string
- *
- *    *@src: source of string
- *
- *     *
- *
- *      *Return: the value dest
- *
- *       */
-
-
+ *Return: the value dest
+ */
 
 char *_strcpy(char *dest, char *src)
-
 {
+int i = 0;
 
-	int i = 0;
-
-
-
-	for (i = 0; src[i]; i++)
-
-		dest[i] = src[i];
-
-	dest[i] = src[i];
-
-	return (dest);
-
+for (i = 0; src[i]; i++)
+dest[i] = src[i];
+dest[i] = src[i];
+return (dest);
 }
