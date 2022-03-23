bd65533118b2ac6caedcfccd829e03d4c8eb16e4
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-23 07:39:14 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,43 +1,22 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ *_strcmp - compare strings
+ *@s1: string
+ *@s2: string
  *
- *  *_strcmp - compare strings
- *
- *   *@s1: string
- *
- *    *@s2: string
- *
- *     *
- *
- *      *Return: integer j
- *
- *       */
-
-
+ *Return: integer j
+ */
 
 int _strcmp(char *s1, char *s2)
-
 {
+int i = 0;
+int j = 0;
 
-	int i = 0;
-
-	int j = 0;
-
-
-
-	while (s1[i] == s2[i] && s1[i] != '\0')
-
-	{
-
-		i++;
-
-	}
-
-	j += s1[i] - s2[i];
-
-	return (j);
-
+while (s1[i] == s2[i] && s1[i] != '\0')
+{
+i++;
+}
+j += s1[i] - s2[i];
+return (j);
 }
