4abe14b9a017da71cff711d56818d0de49b72f47
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-23 07:28:57 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,65 +1,33 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ *_strncat - concatenate strings
+ *@dest: destination
+ *@src: source
+ *@n: integer
  *
- *  *_strncat - concatenate strings
- *
- *   *@dest: destination
- *
- *    *@src: source
- *
- *     *@n: integer
- *
- *      *
- *
- *       *Return: destination
- *
- *        */
-
-
+ *Return: destination
+ */
 
 char *_strncat(char *dest, char *src, int n)
-
 {
+int j = 0;
+int i = 0;
 
-	int j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
 
-	int i = 0;
+for (j = 0; src[j] != '\0'; j++)
+{
+if (j < n)
+{
+dest[j + i] = src[j];
+}
+else
 
-
-
-	while (dest[i] != '\0')
-
-	{
-
-		i++;
-
-	}
-
-
-
-	for (j = 0; src[j] != '\0'; j++)
-
-	{
-
-		if (j < n)
-
-		{
-
-			dest[j + i] = src[j];
-
-		}
-
-		else
-
-
-
-			dest[j + i] = '\0';
-
-	}
-
-	return (dest);
-
+dest[j + i] = '\0';
+}
+return (dest);
 }
