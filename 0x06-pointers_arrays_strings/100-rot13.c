8071da0763439a401b1727a21b685f4bbac63174
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-23 08:20:41 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,57 +1,29 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ *rot13 - encode characters
+ *@k: array of characters
  *
- *  *rot13 - encode characters
- *
- *   *@k: array of characters
- *
- *    *
- *
- *     *Return: array k
- *
- *      */
-
-
+ *Return: array k
+ */
 
 char *rot13(char *k)
-
 {
+char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+int i = 0;
+int j = 0;
 
-	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-
-
-	int i = 0;
-
-	int j = 0;
-
-
-
-	while (k[i] != '\0')
-
-	{
-
-		for (j = 0; a[j] != '\0' && k[i] != a[j]; j++)
-
-			;
-
-		if (j < 52)
-
-		{
-
-			k[i] = b[j];
-
-		}
-
-		i++;
-
-	}
-
-	return (k);
-
+while (k[i] != '\0')
+{
+for (j = 0; a[j] != '\0' && k[i] != a[j]; j++)
+;
+if (j < 52)
+{
+k[i] = b[j];
+}
+i++;
+}
+return (k);
 }
