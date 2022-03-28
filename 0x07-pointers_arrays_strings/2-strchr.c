1f3fa0aefe90dbd1be3fa7047dc4e34c8cf9dd14
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-28 08:04:33 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,43 +1,22 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ *_strchr - locates character in a string
+ *@s: string
+ *@c: character
  *
- *  *_strchr - locates character in a string
- *
- *   *@s: string
- *
- *    *@c: character
- *
- *     *
- *
- *      *Return: 0
- *
- *       */
-
-
+ *Return: 0
+ */
 
 char *_strchr(char *s, char c)
-
 {
 
+do {
+if (*s == c)
+return ((char *)s);
+}
+while (*s++)
+;
 
-
-	do {
-
-		if (*s == c)
-
-			return ((char *)s);
-
-	}
-
-	while (*s++)
-
-		;
-
-
-
-	return (0);
-
+return (0);
 }
