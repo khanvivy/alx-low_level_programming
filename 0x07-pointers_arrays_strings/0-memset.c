2ca36479d6fb7057f66595779fb57e8d6e47f08a
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-28 07:54:52 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,45 +1,23 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ *_memset - fill memory with constant
+ *@s: space of memory
+ *@b: byte
+ *@n: memory area
  *
- *  *_memset - fill memory with constant
- *
- *   *@s: space of memory
- *
- *    *@b: byte
- *
- *     *@n: memory area
- *
- *      *
- *
- *       *Return: pointer to the memory area
- *
- *        */
-
-
+ *Return: pointer to the memory area
+ */
 
 char *_memset(char *s, char b, unsigned int n)
-
 {
+unsigned int i;
+i = 0;
 
-	unsigned int i;
-
-	i = 0;
-
-
-
-	while (i < n)
-
-	{
-
-		*(s + i) = b;
-
-		i++;
-
-	}
-
-	return (s);
-
+while (i < n)
+{
+*(s + i) = b;
+i++;
+}
+return (s);
 }
