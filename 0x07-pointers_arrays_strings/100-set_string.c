c8893069045b0b0c90aed583b2b99b4c2dd2e885
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-28 08:29:49 +0300

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,25 +1,13 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to change
+ * @to: string to change pointer to
  *
- *  * set_string - sets the value of a pointer to a char
- *
- *   * @s: pointer to change
- *
- *    * @to: string to change pointer to
- *
- *     *
- *
- *      * Return: void
- *
- *       */
-
+ * Return: void
+ */
 void set_string(char **s, char *to)
-
 {
-
-	*s = to;
-
+*s = to;
 }
