2e905a48eab9d6b6fe9f55e7ca13374271a5a7b0
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-28 08:25:28 +0300

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,51 +1,26 @@
-#include "holberton.h"
-
+#include "main.h"
 #include <stdio.h>
 
-
-
 /**
+ *print_diagsums - prints the sum of the two diagonals
+ *@a: string
+ *@size: size of square
  *
- *  *print_diagsums - prints the sum of the two diagonals
- *
- *   *@a: string
- *
- *    *@size: size of square
- *
- *     *
- *
- *      *Return: nothing
- *
- *       */
-
+ *Return: nothing
+ */
 void print_diagsums(int *a, int size)
-
 {
+int x, y, sum1, sum2;
+sum1 = 0;
+sum2 = 0;
 
-	int x, y, sum1, sum2;
+for (x = 0; x < size; x++)
 
-	sum1 = 0;
+sum1 += a[(size + 1) * x];
 
-	sum2 = 0;
+for (y = 0; y < size; y++)
 
+sum2 += a[(size - 1) * (y + 1)];
 
-
-	for (x = 0; x < size; x++)
-
-
-
-		sum1 += a[(size + 1) * x];
-
-
-
-	for (y = 0; y < size; y++)
-
-
-
-		sum2 += a[(size - 1) * (y + 1)];
-
-
-
-	printf("%d, %d\n", sum1, sum2);
-
+printf("%d, %d\n", sum1, sum2);
 }
