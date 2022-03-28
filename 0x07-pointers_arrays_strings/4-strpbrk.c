940e2b61f92c9143f783769269589ed76952e420
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-28 08:10:37 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,47 +1,24 @@
-#include "holberton.h"
-
+#include "main.h"
 #include <stdlib.h>
 
-
-
 /**
+ *_strpbrk - locate the first occurence in the string
+ *@s: string
+ *@accept: string
  *
- *  *_strpbrk - locate the first occurence in the string
- *
- *   *@s: string
- *
- *    *@accept: string
- *
- *     *
- *
- *      *Return: NULL
- *
- *       */
-
-
+ *Return: NULL
+ */
 
 char *_strpbrk(char *s, char *accept)
-
 {
+int i = 0;
+int j = 0;
 
-	int i = 0;
-
-	int j = 0;
-
-
-
-	for (i = 0; s[i]; i++)
-
-	{
-
-		for (j = 0; accept[j]; j++)
-
-			if (*(accept + j) == s[i])
-
-				return (&(s[i]));
-
-	}
-
-	return (NULL);
-
+for (i = 0; s[i]; i++)
+{
+for (j = 0; accept[j]; j++)
+if (*(accept + j) == s[i])
+return (&(s[i]));
+}
+return (NULL);
 }
