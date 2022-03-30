25c51ee5f4616e275cc7674a354ac530bdf96020
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-30 08:22:05 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,37 +1,19 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ *_puts_recursion - print string
+ *@s: string
  *
- *  *_puts_recursion - print string
- *
- *   *@s: string
- *
- *    *
- *
- *     *Return: nothing
- *
- *      */
-
-
+ *Return: nothing
+ */
 
 void _puts_recursion(char *s)
-
 {
-
-	if (*s == '\0')
-
-	{
-
-		_putchar('\n');
-
-		return;
-
-	}
-
-	_putchar(s[0]);
-
-	_puts_recursion(s + 1);
-
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(s[0]);
+_puts_recursion(s + 1);
 }
