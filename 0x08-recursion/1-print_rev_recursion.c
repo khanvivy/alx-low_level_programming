2a80441a43fcb8e232cc01c5bff0ed85b123e9c5
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-03-30 08:30:05 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,39 +1,20 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *   * _print_rev_recursion - Print a string in reverse
- *
- *     * @s: the string to reverse
- *
- *       *
- *
- *         * Return: Nothing.
- *
- *           */
-
+  * _print_rev_recursion - Print a string in reverse
+  * @s: the string to reverse
+  *
+  * Return: Nothing.
+  */
 void _print_rev_recursion(char *s)
-
 {
+	if (*s == '\0')
+	{
+		return;
+	}
 
-		if (*s == '\0')
-
-				{
-
-							return;
-
-								}
-
-
-
-			s++;
-
-				_print_rev_recursion(s);
-
-					s--;
-
-						_putchar(*s);
-
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
