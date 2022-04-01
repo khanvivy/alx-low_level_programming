5ac846965dbcf61c868d6740c9123c1809516787
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-04-01 14:37:31 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,33 +1,17 @@
-#include "holberton.h"
-
+#include "main.h"
 #include <stdio.h>
 
-
-
 /**
+ *main - Entry point
+ *@argc: count of the arguments supplied to the program
+ *@argv: array of pointers to the strings
  *
- *  *main - Entry point
- *
- *   *@argc: count of the arguments supplied to the program
- *
- *    *@argv: array of pointers to the strings
- *
- *     *
- *
- *      *Return: 0
- *
- *       */
-
-
+ *Return: 0
+ */
 
 int main(int argc, char *argv[] __attribute__ ((unused)))
-
 {
+printf("%d\n", argc - 1);
 
-	printf("%d\n", argc - 1);
-
-
-
-	return (0);
-
+return (0);
 }
