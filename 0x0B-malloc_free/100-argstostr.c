a73efbdbbf45eb29d2cc90e07fa51a774cd6b25f
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-04-05 18:14:38 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,113 +1,57 @@
 #include <stdlib.h>
-
 #include <stdio.h>
-
 int _strLen(char *);
-
 /**
+ * argstostr - takes all args and concatenates them into str
  *
- *  * argstostr - takes all args and concatenates them into str
+ * @ac: arg count, num of args in av
+ * @av: array of pointers to words (args)
  *
- *   *
- *
- *    * @ac: arg count, num of args in av
- *
- *     * @av: array of pointers to words (args)
- *
- *      *
- *
- *       * Return: pointer to beginning of new string
- *
- *        */
-
+ * Return: pointer to beginning of new string
+ */
 char *argstostr(int ac, char **av)
-
 {
+	int i = 0, n = 0, totalSize = 0;
+	char *newStr, *startNewStr;
 
-		int i = 0, n = 0, totalSize = 0;
+	if (ac <= 0 || av == NULL)
+		return (NULL);
 
-			char *newStr, *startNewStr;
+	while (i < ac)
+	{
+		totalSize += _strLen(av[i]) + 1; /* need space for additional new line */
+		i++;
+	}
+	newStr = malloc(sizeof(char) * totalSize + 1); /* plus null byte */
+	if (newStr == NULL) /* ran out of memory */
+		return (NULL);
+	startNewStr = newStr;
 
-
-
-				if (ac <= 0 || av == NULL)
-
-							return (NULL);
-
-
-
-					while (i < ac)
-
-							{
-
-										totalSize += _strLen(av[i]) + 1; /* need space for additional new line */
-
-												i++;
-
-													}
-
-						newStr = malloc(sizeof(char) * totalSize + 1); /* plus null byte */
-
-							if (newStr == NULL) /* ran out of memory */
-
-										return (NULL);
-
-								startNewStr = newStr;
-
-
-
-									i = 0;
-
-										while (i < ac)
-
-												{
-
-															for (n = 0; av[i][n]; n++)
-
-																		{
-
-																						*newStr++ = av[i][n];
-
-																								}
-
-																	*newStr++ = '\n';
-
-																			i++;
-
-																				}
-
-											*newStr = '\0';
-
-												return (startNewStr);
-
+	i = 0;
+	while (i < ac)
+	{
+		for (n = 0; av[i][n]; n++)
+		{
+			*newStr++ = av[i][n];
+		}
+		*newStr++ = '\n';
+		i++;
+	}
+	*newStr = '\0';
+	return (startNewStr);
 }
-
 /**
+ * _strLen - returns length of string
  *
- *  * _strLen - returns length of string
+ * @s: string to check
  *
- *   *
- *
- *    * @s: string to check
- *
- *     *
- *
- *      * Return: integer, length of string
- *
- *       */
-
+ * Return: integer, length of string
+ */
 int _strLen(char *s)
-
 {
+	int strL = 0;
 
-		int strL = 0;
-
-
-
-			while (*(s + strL))
-
-						strL++;
-
-				return (strL);
-
+	while (*(s + strL))
+		strL++;
+	return (strL);
 }
