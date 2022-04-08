972108ba7832a680eb1cb153047b65f41d12a44f
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-04-08 14:00:01 +0300

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,51 +1,69 @@
-#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
+
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 /**
- * *_realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previsouly allocated by malloc
- * @old_size: size of the allocated memory for ptr
- * @new_size: new size of the new memory block
+ * _realloc - allocates memory block using malloc and free
+ * @ptr: pointer to the memory previosly allocated with malloc
+ * @old_size: The size of the allocated space of ptr
+ * @new_size: The new size to allocate
  *
- * Return: pointer to the newly allocated memory block
+ * Description: allocates a new memory block for the pointer,
+ * using the contents from the original pointer, copiyng up to the
+ * minimum of the old and new sizes.
+ * If new_size > old_size, the added memory should not be intialized
+ * If new_size == old_size, returns the same pointer
+ * If ptr == NULL, call is equivalent to malloc(new_size)
+ * If new_size == 0 and ptr != NULL, call is equivalent to free(ptr),
+ *  and return NULL.
+ *
+ * Return: A pointer to the new allocated memory and free ptr.
+ * NULL if can not allocate memory
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *nptr;
-	unsigned int i;
+	char *p;
 
 	if (new_size == old_size)
 		return (ptr);
 
-	if (ptr == NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
-		nptr = malloc(new_size);
-
-		if (nptr == NULL)
-			return (NULL);
-
-		return (nptr);
-	}
-	else
-	{
-		if (new_size == 0)
-		{
-			free(ptr);
-			return (NULL);
-		}
+		free(ptr);
+		return (NULL);
 	}
 
-	nptr = malloc(new_size);
+	p = malloc(new_size);
 
-	if (nptr == NULL)
+	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < old_size && i < new_size; i++)
+	if (ptr == NULL)
+		return (p);
+
+	p = _memcpy(p, ptr, (new_size > old_size ? old_size : new_size));
+	free(ptr);
+	return (p);
+}
+
+/**
+ * _memcpy - copies the memory are from
+ * src to dest
+ * @dest: The destination pointer
+ * @src: The source pointer
+ * @n: bytes to use from src
+ *
+ * Return: The pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
 	{
-		nptr[i] = ((char *) ptr)[i];
+		*(dest + i) = *(src + i);
+		i++;
 	}
 
-	free(ptr);
-	return (nptr);
+	return (dest);
 }
