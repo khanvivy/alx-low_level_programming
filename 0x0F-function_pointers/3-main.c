1cdec00d3c1b7877141a31751c11fc6223743020
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-04-13 13:21:51 +0300

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,30 +1,36 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "3-calc.h"
+
 /**
- * main - program to perform simple operations
+ * main - entry point
  * @argc: argument count
- * @argv: array of arguments
- * Return: exits 0 normally, 98 on argc error, 99 on illegal operator
+ * @argv: argument vector
+ *
+ * Return: the job has been finished
  */
+
 int main(int argc, char *argv[])
 {
-	int (*func)(int, int);
+	int v;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	func = get_op_func(argv[2]);
-	if (func == NULL)
+	if ((argv[2][1] != 0) || ((argv[2][0] != '+') &&  (argv[2][0] != '-')
+		&& (argv[2][0] != '*') && (argv[2][0] != '/') && (argv[2][0] != '%')))
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+	if ((argv[2][0] == '/' || (argv[2][0] == '%')) && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	v = get_op_func(argv[2]) (atoi(argv[1]), atoi(argv[3]));
+			printf("%d\n", v);
 	return (0);
 }
