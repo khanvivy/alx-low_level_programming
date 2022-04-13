4d1dae7aec65e4d2f7cb119a201b0f969229adc9
khanvivy <vivykhan@gmail.com>
2022-04-13 00:09:02 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * print_name_is - function that prints a name
+ * @name - name of the person
+ *
+ * Return: nothing
+ */
+void print_name_is(char *name)
+{
+	printf("Hello, my name is %s\n", name);
+}
+
+/**
+ * main - check the code
+ *
+ * Return:0
+ */
+int main(void)
+{
+	printf("Viola", print_name_is);
+	printf("\n");
+	return (0);
+}
+
+
