a0a2ed3852bccfcf05dace7ac289c4d1e74bb883
khanvivy <vivykhan@gmail.com>
2022-04-13 03:07:34 -0700

    3-main.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,10 @@
+#ifndef _3_op_functions
+#define _3_op_functions
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+#endif
