dc1415ebc574f1f4261b09ccd0972ca59f0e8c69
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-04-13 13:27:35 +0300

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,10 +1,67 @@
-#ifndef _3_op_functions
-#define _3_op_functions
+#include "3-calc.h"
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+/**
+ * op_add - addition operation
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
 
-#endif
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction operation
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication operation
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division operation
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - modulo operation
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
