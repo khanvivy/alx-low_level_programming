9976c7a6e9929695fff3e9ed6d421ee68ee970c6
khanvivy <46683276+khanvivy@users.noreply.github.com>
2022-04-21 15:47:28 +0300

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,37 +1,52 @@
-#include "lists.h"
 #include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
 /**
- * add_node_end - A function that adds a node to the end of linked list
- * @head: pointer to a list_t pointer that points to the head struct
- * @str: string to add as node
- * Return: pointer to new element of list, NULL on failure
- */
+ * _strlen - Length of the passed string
+ * @s: String
+ * Return: Returns the length of a string
+**/
+int _strlen(const char *s)
+{
+int i = 0;
+
+while (*(s + i) != '\0')
+i++;
+
+return (i);
+}
+
+/**
+ * add_node_end - Add nodes to the end of the nodes
+ * @head: Pointer to a struct pointer
+ * @str: String passed
+ * Return: Returns amount of node
+**/
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *temp;
-	unsigned int length = 0;
+list_t *newNode, *tmp = *head;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
+newNode = malloc(sizeof(newNode));
+if (newNode == NULL)
+return (NULL);
+if (str == NULL)
+{
+free(newNode);
+return (NULL);
+}
+newNode->len = _strlen(str);
+newNode->str = strdup(str);
+newNode->next = NULL;
+if (*head == NULL)
+{
+*head = newNode;
+return (newNode);
+}
+while (tmp->next != NULL)
+tmp = tmp->next;
+tmp->next = newNode;
 
-	while (str[length])
-		length++;
-
-	new_node->len = length;
-	new_node->str = strdup(str);
-	if (*head == NULL)
-	{
-		new_node->next = *head;
-		*head = new_node;
-	}
-	else
-	{
-		new_node->next = NULL;
-		temp = *head;
-		while (temp->next)
-			temp = temp->next;
-		temp->next = new_node;
-	}
-	return (new_node);
+return (newNode);
 }
