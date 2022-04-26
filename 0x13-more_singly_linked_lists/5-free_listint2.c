48441337a6bfc5ab0fc724626e50ef36c7899f60
Viola <vivykhan@gmail.com>
2022-04-25 22:59:41 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,12 +12,12 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 		return;
 
-	while (*head != NULL)
-		{
-			tmp = *head;
-			*head = (*head)->next;
-			free(tmp);
-		}
-
+	temp = *head;
+	while (*head)
+	{
+		temp = *head;
+		(*head) = (*head)->next;
+		free(temp);
+	}
 	*head = NULL;
 }
